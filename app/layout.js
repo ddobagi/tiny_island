c783da7e8796aa418ccab9485b1e552828093e6c
ddobagi <freshcom@snu.ac.kr>
2025-02-28 01:47:40 +0900

    이번에는 jsconfig.json도

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SheetsProvider } from "@/context/SheetsContext";
+import { SheetsProvider } from "../context/SheetsContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
