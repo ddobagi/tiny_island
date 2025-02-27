d353d00b72040a9d8e77092716f661c536c0e81c
ddobagi <freshcom@snu.ac.kr>
2025-02-28 01:52:19 +0900

    확장자 포함

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SheetsProvider } from "../context/SheetsContext";
+import { SheetsProvider } from "@/context/SheetsContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,12 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <SheetsProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          {children}
-        </body>
-      </html>
-    </SheetsProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
