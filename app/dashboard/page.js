d15dbc4681b0378e7848b98e39bca3fa05bfd520
ddobagi <freshcom@snu.ac.kr>
2025-03-02 00:32:13 +0900

    대시보드 디자인 수정

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -170,7 +170,7 @@ export default function Dashboard() {
         </div>
       </div>      
 
-      <div className="fixed bottom-6 right-6 flex flex-col items-end" ref={fabRef}>
+      <div className="z-10 fixed bottom-6 right-6 flex flex-col items-end" ref={fabRef}>
         {fabOpen && (
           <div className="relative px-4 py-2 w-[400px] transition-transform transform translate-y-2 opacity-100 mb-2">
             <p className="font-pretendard font-semibold text-gray-700">URL</p>
@@ -180,7 +180,7 @@ export default function Dashboard() {
                 placeholder="Youtube URL" 
                 value={newVideo.video} 
                 onChange={handleInputChange} 
-                className="z-10 flex-1 bg-gray-100 focus:outline-none text-gray-700" 
+                className="flex-1 bg-gray-100 focus:outline-none text-gray-700" 
               />
               <Button 
                 onClick={handleAddVideo} 
@@ -211,7 +211,7 @@ export default function Dashboard() {
                   className="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full shadow-md hover:bg-red-600">
                   <Trash2 size={32} />
                 </button>
-                <div className = "h-72 rounded-lg shadow-lg cursor-pointer hover:shadow-2xl transition">
+                <div className = "rounded-lg shadow-lg cursor-pointer hover:shadow-2xl transition">
                   <img src={video.thumbnail} alt={video.name} className="w-full h rounded-t-lg object-cover" />
                 </div >
                 <CardContent className="p-4">
