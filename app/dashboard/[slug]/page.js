3dfdc56c00b60b7519d23354d6478702a64f6499
ddobagi <freshcom@snu.ac.kr>
2025-03-05 19:15:01 +0900

    private 모드에서의 likes/videoId 페이지

--- app/dashboard/[slug]/page.js
+++ app/dashboard/[slug]/page.js
@@ -79,9 +79,15 @@ export default function VideoDetail() {
         const userId = auth.currentUser?.uid;
         console.log("2번 uid 통과");
 
-        let docRef = mode
-          ? doc(db, "gallery", slug) : doc(db, "users", userId, "videos", slug)
-
+        let docRef;
+        if (previousPage === "/dashboard/likes") {
+          docRef = doc(db, "gallery", slug);
+        } else {
+          docRef = mode
+              ? doc(db, "gallery", slug) 
+              : doc(db, "users", userId, "videos", slug);
+        }
+        
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
